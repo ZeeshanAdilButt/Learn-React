3aa14152436f6e487fc629a5847e68f57d9f0ab6
Zeeshan <zeeabutt@outlook.com>
2019-06-09 04:22:08 +0500

    added a test array

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,8 @@ class Counter extends Component {
   state = {
     count: 0,
     imageUrl: "https://picsum.photos/200",
-    tags: ["tag 1", "tag 2", "tag 3"]
+    tags: ["tag 1", "tag 2", "tag 3"],
+    testArray: []
   };
 
   styles = {
