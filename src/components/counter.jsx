f9a11385433211af60f0b0ba3487cb12da4cee21
Zeeshan <zeeabutt@outlook.com>
2019-06-09 05:23:23 +0500

    passing data between components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import { render } from "react-dom";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     imageUrl: "https://picsum.photos/200",
     tags: ["tag 1", "tag 2", "tag 3"],
     testArray: []
@@ -14,33 +15,36 @@ class Counter extends Component {
 
   renderTags() {
     if (this.state.tags.length === 0) return <p> There are no tags here</p>;
-
     return this.state.tags.map(tag => <li key={tag}> {tag} </li>);
   }
 
   handleIncrement = product => {
     // console.log("Increment Clicked", this);
-
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    console.log("props", this.props);
+
     return (
       <React.Fragment>
         {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span style={{ fontSize: 10 }} className={this.getBadgeClasses()}>
+        {this.props.children}
+        <span style={{ fontSize: 13 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
+
         <button
           onClick={() => this.handleIncrement({ product: 1 })}
           className="btn btn-sm btn-secondary"
         >
-          {" "}
-          Increment{" "}
+          Increment
         </button>
+
         <br />
         {this.state.tags.length === 0 && "Please create a new tag"}
+
         <ul>{this.renderTags()}</ul>
       </React.Fragment>
     );
@@ -48,12 +52,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
 
     return count === 0 ? "ZERO" : count;
   }
